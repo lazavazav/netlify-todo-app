b0bd8bdee642f6632840903b669f07d583e2896f
Brian Morrison II <brian@brianmorrison.me>
2021-03-16 15:22:30 -0500

    Updated hello world component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="My Todo App in Netlify!"/>
   </div>
 </template>
 
